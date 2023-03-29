a140f556ea04ab38c0d005ba875f69bd4a4b6858
Osilaja78 <osilajaabdulhameed@gmail.com>
2023-03-29 08:33:23 +0100

    modified main.py file

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ app = FastAPI(title="MathEase")
 
 # Enable CORS middleware
 origins = [
-    "https://r6mjxn.deta.dev"
     "http://localhost",
     "http://localhost:8080",
     "http://localhost:3000",
@@ -30,7 +29,7 @@ models.Base.metadata.create_all(bind=engine)
 # Index page
 @app.get('/')
 def index():
-    return {'message': 'Welcome to MathBuddy!'}
+    return {'message': 'Welcome to MathEase!'}
 
 # Include routes from other router files
 app.include_router(users.router)
